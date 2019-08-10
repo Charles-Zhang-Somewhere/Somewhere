ac273d45a9df4c29158887b5423fd5bb6e6147e1
Charles Zhang <szinubuntu@gmail.com>
2019-08-10 10:03:19 -0400

    Project setup; Run desktop if no command line argument is given to Somewhere console program
    - Configure **output directory**
    - Change framework dependency to netcore2.2 to enable generating **framework dependent executable**
    - Add ignore **output directory**
    - Add publish profiles

--- Somewhere/Program.cs
+++ Somewhere/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Somewhere
 {
@@ -6,7 +7,34 @@ namespace Somewhere
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Print working directory for identification
+            Console.WriteLine($"Working Directory: {Directory.GetCurrentDirectory()}");
+
+            // Summon desktop UI if no argument is given
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Run desktop.");
+                RunDesktop();
+            }
+
+            // Exit
+            return;
+        }
+
+        private static void RunDesktop()
+        {
+            using (System.Diagnostics.Process pProcess = new System.Diagnostics.Process())
+            {
+                pProcess.StartInfo.FileName = @"SomewhereDesktop";  // Relative to assembly location, not working dir
+                pProcess.StartInfo.Arguments = ""; //argument
+                // pProcess.StartInfo.UseShellExecute = false;
+                // pProcess.StartInfo.RedirectStandardOutput = true;
+                // pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                // pProcess.StartInfo.CreateNoWindow = true; // not diplay a windows
+                pProcess.Start();
+                // string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                // pProcess.WaitForExit();
+            }
         }
     }
 }
