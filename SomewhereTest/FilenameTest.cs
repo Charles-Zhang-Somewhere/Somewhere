f1dd2e07865948efb50be9bf83dd679cc09e7c79
Charles Zhang <szinubuntu@gmail.com>
2019-09-03 14:28:51 -0400

    [Desktop] (Code Organization) Add reference to SQLite library to accomodate for new Commands class API change

---
+++ SomewhereTest/FilenameTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Somewhere;
+
+namespace SomewhereTest
+{
+    /// <summary>
+    /// A suite of tests dedicated to handle filename cases
+    /// </summary>
+    public class FilenameTest
+    {
+        /// <summary>
+        /// When changing the name of a managed file to include folder path, it should create proper subfolder hierarchies for the file
+        /// </summary>
+        [Fact]
+        public void MoveShouldCreateSubfolder()
+        {
+            Helper.CleanOrCreateTestFolderRemoveAllFiles();
+            Commands Commands = Helper.CreateNewCommands();
+            Commands.New();
+            Commands.Doc("test.txt");
+            Commands.MV("test.txt", "folder1/folder2\\test*.txt");   // Name containing invalid characters
+            // Clean up
+            Commands.Dispose();
+            Helper.CleanTestFolderRemoveAllFiles();
+        }
+
+    }
+}
